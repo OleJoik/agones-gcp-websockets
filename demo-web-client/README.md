8fc864b73cc46ca0e71b05fe3229e2a88416d558
Ole-Jakob <ole.jakob.olsen@gmail.com>
2022-07-18 23:39:12 +0200

    web client before adding websockets

---
+++ demo-web-client/README.md
@@ -0,0 +1,6 @@
+# Demo web client
+
+Try the client online: https://agones-websocket-demo.netlify.app/
+
+Hosted at [netlify](https://app.netlify.com/sites/agones-websocket-demo/)
+
--- demo-web-client/index.js
+++ demo-web-client/index.js
@@ -3,7 +3,63 @@ import { createApp } from 'vue'
 const app = {
   data(){
     return {
-      message: "Hello world"
+      connected: false,
+      url: null,
+      alias: null,
+      messageInput: null,
+      messages: [
+        "Old message 1",
+        "Old message 2",
+        "Old message 3",
+      ]
+    }
+  },
+  watch: {
+    messages: {
+      handler(){
+        this.scrollToBottom();
+      },
+      deep: true,
+      flush: 'post'
+    }
+  },
+  computed: {
+    buttonText(){
+      let text = "Connect"
+      if(this.connected) text = "Disconnect"
+      return text
+    }
+  },
+  methods: {
+    connectClicked(){      
+      if(!this.validateUrl()) return;
+      if(!this.validateAlias()) return;
+
+      this.connected = !this.connected;
+    },
+
+    sendMessage(){
+      this.messages.push(this.messageInput);
+      this.messageInput = null;
+    },
+
+    scrollToBottom(){
+      this.$refs.messageElement.scrollTop = this.$refs.messageElement.scrollHeight;
+    },
+
+    validateUrl(){
+      let valid = /^[a-zA-Z0-9-]+\.[a-zA-Z0-9\d-]+\.[a-z]+$/.test(this.url || "")
+      if(!valid) alert("Not a valid game server URL")
+
+      return valid;
+    },
+
+    validateAlias(){
+      console.log(this.alias);
+      let valid = /^[a-zA-Z]\w*$/.test(this.alias || "")
+      if(!valid) alert("Not a valid alias.")
+    
+      return valid;
     }
   }
 }
